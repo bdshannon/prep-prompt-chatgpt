f8681f23605e76346738dd68f89e29f1e8869330
Brian Shannon <git@bshannon.net>
2023-05-13 15:40:22 +0100

    settings, upd form selector

--- populate-text-field.js
+++ populate-text-field.js
@@ -1,13 +1,23 @@
 
-let form = document.getElementsByTagName("form")?.[0];
-let textField = form?.getElementsByTagName("textarea")?.[0];
-const promptAssistText = "Correct the following French input and then provide a response in French. ";
+textareaSelector = "textarea[placeholder=\"Send a message.\"]";
+let form = document.querySelector("form:has(" + textareaSelector + ")");
+let textField = form?.querySelector(textareaSelector);
+let promptAssistText = "Correct the following French input and then provide a response in French.";
 
 function populateTextField() {
 	if(!textField.value.startsWith(promptAssistText)) {
-		textField.value = promptAssistText + textField.value;
+		textField.value = promptAssistText + " " + textField.value;
 	}
 }
 
-populateTextField();
-textField.addEventListener("input", populateTextField);
+chrome.storage.sync.get(["prompt"]).then(function(data) {
+	if (data.prompt) {
+		promptAssistText = data.prompt;
+	}
+}).then(function() {
+	if(textField) {
+		setTimeout(populateTextField, 5000);
+	}
+});
+
+textField?.addEventListener("input", populateTextField);
---
+++ settings.js
@@ -0,0 +1,15 @@
+var textField = document.getElementById("text-field");
+
+chrome.storage.sync.get(["prompt"]).then(function(data) {
+  if (data.prompt) {
+    textField.value = data.prompt;
+  }
+});
+
+document.getElementById("settings-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  chrome.storage.sync.set({ "prompt": textField.value }).then(function() {
+    console.log("prompt saved.");
+	document.querySelector("p[data-new-prompt]").setAttribute("data-new-prompt", textField.value);
+  });
+});
